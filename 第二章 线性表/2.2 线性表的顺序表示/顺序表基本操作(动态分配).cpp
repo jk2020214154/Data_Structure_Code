67abe5a474cab497c42b33c8285b97e08eb8dd18
Liao Yin <1691156671@qq.com>
2023-07-18 13:52:31 +0800

    version2.0

--- 第二章 线性表/2.2 线性表的顺序表示/顺序表基本操作(动态分配).cpp
+++ 第二章 线性表/2.2 线性表的顺序表示/顺序表基本操作(动态分配).cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include <stdlib.h>
 using namespace std;
+
+#define InitSize 5
 
 typedef int ElemType;
 
@@ -10,9 +13,145 @@ typedef struct{
 }SeqList;
 
 
+void InitList(SeqList &L)//初始化
+{
+    L.data=(ElemType*)malloc(InitSize*sizeof(ElemType));
+    if(L.data==NULL)
+        exit(1);
+    L.length=0;
+    L.MaxSize=InitSize;
+}
+
+bool IncreaseSize(SeqList &L,int len)//增加动态数组长度(辅助函数)
+{
+    ElemType *p=L.data;
+    L.data=(ElemType*)malloc((L.MaxSize+len)*sizeof(ElemType));
+
+    if(L.data==NULL)
+        return false;
+    for(int i=0;i<L.length;i++)
+        L.data[i]=p[i];
+    
+    L.MaxSize=L.MaxSize+len;
+    free(p);
+    return true;
+}
+
+bool ListInsert(SeqList &L,int i,ElemType e)//插入操作
+{
+    if(i<1||i>L.length+1)
+        return false;
+    if(L.length>=L.MaxSize)
+    {
+        bool status=IncreaseSize(L, 10);
+        if(status==false)
+            return false;
+    }
+
+    for(int j=L.length;j>=i;j--)
+        L.data[j]=L.data[j-1];
+    
+    L.data[i-1]=e;
+    L.length++;
+    return true;
+}
+
+
+bool ListDelete(SeqList &L,int i,ElemType &e)//删除操作
+{
+    if(i<1||i>L.length)
+        return false;
+    
+    e=L.data[i-1];
+    for(int j=i;j<L.length;j++)
+        L.data[j-1]=L.data[j];
+    L.length--;
+    return true;
+}
+
+
+ElemType GetElem(SeqList L,int i)//按位查找
+{
+    if(i>=1&&i<=L.length)
+        return L.data[i-1];
+    else return INT_MIN;
+}
+
+int LocateElem(SeqList L,ElemType e)//按值查找第一个值为e的位置
+{
+    for(int i=0;i<L.length;i++)
+        if(L.data[i]==e)
+            return i+1;
+    return -1;
+}
+
+int getLength(SeqList L)//获取线性表的长度
+{
+    return L.length;
+}
+
+void PrintList(SeqList L)//打印线性表的数据元素
+{
+    for(int i=0;i<L.length;i++)
+        cout << L.data[i] << " ";
+    cout << endl;
+}
+
+bool Empty(SeqList L)//判断线性表是否为空
+{
+    return L.length==0;
+}
+
+void DestroyList(SeqList &L)//销毁线性表
+{
+    free(L.data);
+    L.length=0;
+}
+
+
+
+void Test()
+{
+    SeqList L;
+    InitList(L);
+
+    cout << Empty(L) << endl;
+
+    ListInsert(L, 1, 86);
+    PrintList(L);
+
+    ListInsert(L, 2, 8);
+    PrintList(L);
+
+    ListInsert(L, 2, 5);
+    PrintList(L);
+
+    ListInsert(L, 1, 14);
+
+    ListInsert(L, 5, 6);
+    ListInsert(L, 1, 23);
+
+    cout << GetElem(L, 100) << endl;
+    cout << LocateElem(L, 8) << endl;
+
+    cout << L.MaxSize << endl;
+
+    PrintList(L);
+
+    ElemType num;
+    ListDelete(L, 3, num);
+
+    cout << num << endl;
+    PrintList(L);
+
+    cout << getLength(L) << endl;    
+
+    DestroyList(L);
+}
+
+
 int main()
 {
-
-
-
+    Test();
+    return 0;
 }
--- 第二章 线性表/2.2 线性表的顺序表示/顺序表基本操作(静态分配).cpp
+++ 第二章 线性表/2.2 线性表的顺序表示/顺序表基本操作(静态分配).cpp
@@ -13,6 +13,14 @@ typedef struct{
 }SqList;
 
 
+void InitList(SqList &L)//初始化
+{
+    for(int i=0;i<MaxSize;i++)
+        L.data[i]=0;
+    L.length=0;
+}
+
+
 bool ListInsert(SqList &L,int i,ElemType e)//插入操作
 {
     if(i<1||i>L.length+1)
@@ -55,15 +63,65 @@ int LocateElem(SqList L,ElemType e)//按值查找第一个值为e的位置
     return -1;
 }
 
+int getLength(SqList L)//获取线性表的长度
+{
+    return L.length;
+}
+
+void PrintList(SqList L)//打印线性表的数据元素
+{
+    for(int i=0;i<L.length;i++)
+        cout << L.data[i] << " ";
+    cout << endl;
+}
+
+bool Empty(SqList L)//判断线性表是否为空
+{
+    return L.length==0;
+}
+
+void DestroyList(SqList &L)//销毁线性表
+{
+    ;//由于是数组可不用进行销毁
+}
 
 
+void Test()
+{
+    SqList L;
 
+    InitList(L);
 
+    cout << Empty(L) << endl;
+
+    ListInsert(L, 1, 86);
+    PrintList(L);
+
+    ListInsert(L, 2, 8);
+    PrintList(L);
+
+    ListInsert(L, 2, 5);
+    PrintList(L);
+
+    ListInsert(L, 1, 14);
+
+    cout << GetElem(L, 100) << endl;
+    cout << LocateElem(L, 8) << endl;
+
+    PrintList(L);
+
+    ElemType num;
+    ListDelete(L, 3, num);
+
+    cout << num << endl;
+    PrintList(L);
+
+    cout << getLength(L) << endl;
+}
 
 
 int main()
 {
-
-
-
+    Test();
+    return 0;
 }
