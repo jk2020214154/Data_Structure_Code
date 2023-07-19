6ee6e131874baad957bc8aff7571fd85849505b2
Liao Yin <1691156671@qq.com>
2023-07-19 22:52:35 +0800

    实现单链表(带头结点)的基本操作

---
+++ 第二章 线性表/2.3 线性表的链式表示/单链表的基本操作不带头结点.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+typedef int ElemType;
+
+typedef struct LNode{
+    ElemType data;
+    struct LNode *next;
+}LNode,*LinkList;
+
+
+
+void Test()
+{
+
+}
+
+
+int main()
+{
+    Test();
+    return 0;
+}
