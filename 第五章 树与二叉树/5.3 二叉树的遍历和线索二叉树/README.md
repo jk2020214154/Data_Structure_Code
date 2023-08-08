3085d7dedfb2617d4def33f160c174fd977e2b0e
Liao Yin <1691156671@qq.com>
2023-08-08 22:27:35 +0800

    更新5.3

--- 第五章 树与二叉树/5.3 二叉树的遍历和线索二叉树/README.md
+++ 第五章 树与二叉树/5.3 二叉树的遍历和线索二叉树/README.md
@@ -2,7 +2,17 @@
 
 * 递归版
 
-5555
+```cpp
+void PreOrder(BiTree T)//前序遍历
+{
+    if(T!=NULL)
+    {
+        cout << T->value << " ";//访问根节点
+        PreOrder(T->lchild);//递归遍历左子树
+        PreOrder(T->rchild);//递归遍历右子树
+    }
+}
+```
 
 * 非递归版
 
@@ -12,7 +22,17 @@
 
 * 递归版
 
-
+```cpp
+void InOrder(BiTree T)//中序遍历
+{
+    if(T!=NULL)
+    {
+        InOrder(T->lchild);//递归遍历左子树
+        cout << T->value << " ";//访问根节点
+        InOrder(T->rchild);//递归遍历右子树
+    }
+}
+```
 
 * 非递归版
 
@@ -22,7 +42,17 @@
 
 * 递归版
 
-
+```cpp
+void PostOrder(BiTree T)//后序遍历
+{
+    if(T!=NULL)
+    {
+        PostOrder(T->lchild);//递归遍历左子树
+        PostOrder(T->rchild);//递归遍历右子树
+        cout << T->value << " ";//访问根结点
+    }
+}
+```
 
 * 非递归版
 
---
+++ 第五章 树与二叉树/5.3 二叉树的遍历和线索二叉树/二叉树的先序遍历.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+
+using namespace std;
+
+#define MaxSize 110
+
+typedef int ElemType;
+
+typedef struct BiTNode{
+    ElemType value;//结点中的顺序元素
+    struct BiTNode *lchild,*rchild;    
+    //struct BiTNode *parent;
+}BiTNode,*BiTree;
+
+void InitTree(BiTree &T)
+{
+    T=NULL;//初始化可将根节点置空，空树
+    T=(BiTree)malloc(sizeof(BiTree));
+
+    T->value=1;
+    T->lchild=NULL;
+    T->rchild=NULL;
+}
+
+bool InsertLeftTreeNode(BiTNode *&T,ElemType e)
+{
+    BiTNode *p=(BiTNode *)malloc(sizeof(BiTNode));
+    if(p==NULL)
+        return false;
+    p->value=e;
+    p->lchild=NULL;
+    p->rchild=NULL;
+    T->lchild=p;
+    return true;
+}
+
+bool InsertRightTreeNode(BiTNode *&T,ElemType e)
+{
+    BiTNode *p=(BiTNode *)malloc(sizeof(BiTNode));
+    if(p==NULL)
+        return false;
+    p->value=e;
+    p->lchild=NULL;
+    p->rchild=NULL;
+    T->rchild=p;
+    return true;    
+}
+
+
+void PreOrder(BiTree T)//前序遍历
+{
+    if(T!=NULL)
+    {
+        cout << T->value << " ";//访问根节点
+        PreOrder(T->lchild);//递归遍历左子树
+        PreOrder(T->rchild);//递归遍历右子树
+    }
+}
+
+
+void Test()
+{
+    BiTree T;
+    InitTree(T);
+    InsertLeftTreeNode(T, 2);
+    InsertRightTreeNode(T, 3);
+    InsertLeftTreeNode(T->lchild, 4);
+    InsertRightTreeNode(T->lchild, 5);
+    InsertLeftTreeNode(T->rchild, 6);
+    InsertRightTreeNode(T->rchild, 7);
+
+    cout << "前序遍历结果:";
+    PreOrder(T);
+
+
+}
+
+int main()
+{
+    Test();
+    return 0;
+}
